ad9be6ae3bf1160b5df90275d5c29e35916197d8
Bhavya-Anupoju <67477724+Bhavya-Anupoju@users.noreply.github.com>
2024-07-29 21:54:41 +0530

    Update Body.js

--- blueberry/src/components/Body.js
+++ blueberry/src/components/Body.js
@@ -19,8 +19,8 @@ const Body = () => {
   const fetchData = async () => {
     try {
       const data = await fetch(
-        "https://www.swiggy.com/dapi/restaurants/list/v5?lat=17.37240&lng=78.43780&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING" ||
-          "https://www.swiggy.com/mapi/homepage/getCards?lat=17.37240&lng=78.43780"
+        "https://corsproxy.io/?https://www.swiggy.com/dapi/restaurants/list/v5?lat=17.37240&lng=78.43780&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING" ||
+          "https://corsproxy.io/?https://www.swiggy.com/mapi/homepage/getCards?lat=17.37240&lng=78.43780"
       );
 
       if (!data.ok) {
